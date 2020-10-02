ccfae7319751bd503e521fbebda24b33000d4c74
Anna Puchkova <48117633+annapuchkova@users.noreply.github.com>
2020-10-02 17:10:40 +0400

    todo

---
+++ precache-manifest.a11018a77c8446a7784dee127864979a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d31dd616404f2fa3d39c42235fe399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2437c754159f2c7cb062",
+    "url": "/static/css/main.df9395d8.chunk.css"
+  },
+  {
+    "revision": "6b8eb765f4de97e4787d",
+    "url": "/static/js/2.c4e79ecb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4e79ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2437c754159f2c7cb062",
+    "url": "/static/js/main.40ea2cab.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
